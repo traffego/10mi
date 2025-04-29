37531b1aa64e6b82ebc30c0b45f43e3592a63651
traffego <traffego.mkt@gmail.com>
2025-04-29 14:45:26 -0300

    Visual Dashboard Com CARDS 4

--- dashboard/home.php
+++ dashboard/home.php
@@ -511,9 +511,14 @@ tbody {
 $status = isset($_GET['status']) ? $_GET['status'] : '';
 $stat_arr = ['Pending Orders', 'Packed Orders', 'Our for Delivery', 'Completed Order']
 ?>
-<h4 class="my-6 font-semibold text-gray-700 dark:text-gray-200">
-  Últimos pedidos
-</h4>
+<div class="flex justify-between items-center my-6">
+  <h4 class="font-semibold text-gray-700 dark:text-gray-200">
+    Últimos pedidos
+  </h4>
+  <a href="./?page=orders" class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+    Ver Todos
+  </a>
+</div>
 <div class="w-full overflow-hidden rounded-lg shadow-xs">
 
   <div class="w-full overflow-x-auto">
